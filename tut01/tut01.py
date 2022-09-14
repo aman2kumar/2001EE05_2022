2b304fd0c1576073f3c497d064dcf6438212271b
Aman Kumar <amankumar33152@gmail.com>
2022-09-14 22:05:16 +0530

    Added the code to find octant count in mod ranges

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -8,21 +8,21 @@ import pandas as pd
 # Function to find the octant value and label it for a particular row
 # taken row as an input
 def label_octant (row):
-    if row['U'] >= 0 and row["V"] >= 0 and row["W"] >= 0:
+    if row["U' = U - U_avg"] >= 0 and row["V' = V - V_avg"] >= 0 and row["W' = W - W_avg"] >= 0:
         return 1
-    elif row['U'] < 0 and row["V"] >= 0 and row["W"] >= 0:
+    elif row["U' = U - U_avg"] < 0 and row["V' = V - V_avg"] >= 0 and row["W' = W - W_avg"] >= 0:
         return 2
-    elif row['U'] < 0 and row["V"] < 0 and row["W"] >= 0:
+    elif row["U' = U - U_avg"] < 0 and row["V' = V - V_avg"] < 0 and row["W' = W - W_avg"] >= 0:
         return 3
-    elif row['U'] >= 0 and row["V"] < 0 and row["W"] >= 0:
+    elif row["U' = U - U_avg"] >= 0 and row["V' = V - V_avg"] < 0 and row["W' = W - W_avg"] >= 0:
         return 4
-    elif row['U'] >= 0 and row["V"] >= 0 and row["W"] < 0:
+    elif row["U' = U - U_avg"] >= 0 and row["V' = V - V_avg"] >= 0 and row["W' = W - W_avg"] < 0:
         return -1
-    elif row['U'] < 0 and row["V"] >= 0 and row["W"] < 0:
+    elif row["U' = U - U_avg"] < 0 and row["V' = V - V_avg"] >= 0 and row["W' = W - W_avg"] < 0:
         return -2
-    elif row['U'] < 0 and row["V"] < 0 and row["W"] < 0:
+    elif row["U' = U - U_avg"] < 0 and row["V' = V - V_avg"] < 0 and row["W' = W - W_avg"] < 0:
         return -3
-    elif row['U'] >= 0 and row["V"] < 0 and row["W"] < 0:
+    elif row["U' = U - U_avg"] >= 0 and row["V' = V - V_avg"] < 0 and row["W' = W - W_avg"] < 0:
         return -4
 
 # Original function for octant identification
@@ -49,6 +49,41 @@ def octact_identification(mod = 5000):
     # Constructing a new column of Octant and applying logic given below
     # Using lambda function on a particular row to find its octant via pre defined function (label_octant())
     df["Octant"] = df.apply(lambda row: label_octant(row), axis = 1)
+
+    # Adding Column of User Input
+    df.at[1, 11] = 'User Input'
+
+    # Calculated total rows in dataframe to get the iteration of the for loop
+    total_rows = len(df.axes[0])
+    iteration = total_rows/mod
+
+    # For loop to give the count of octants in the ranges of given mod value
+    for i in range(int(iteration) + 1):
+
+        # Divided original dataframe in range based on no. of loops
+        # ex- for loop1 range = 0 to 5000, loop2 range = 5001 to 10000 and so on..
+        df_range = df[i*mod: (i + 1)*mod]
+
+        # Calculated counts of different octant values for particular range
+        count_1 = len((df_range[df_range["Octant"] == 1]).axes[0])
+        count_neg1 = len((df_range[df_range["Octant"] == -1]).axes[0])
+        count_2 = len((df_range[df_range["Octant"] == 2]).axes[0])
+        count_neg2 = len((df_range[df_range["Octant"] == -2]).axes[0])
+        count_3 = len((df_range[df_range["Octant"] == 3]).axes[0])
+        count_neg3 = len((df_range[df_range["Octant"] == -3]).axes[0])
+        count_4 = len((df_range[df_range["Octant"] == 4]).axes[0])
+        count_neg4 = len((df_range[df_range["Octant"] == -4]).axes[0])
+
+        # Added the value of counts in respective rows and columns 
+        df.at[i + 2, 'Octant ID'] = str(i*mod) + ' - ' + str((i+1)*mod)
+        df.at[i + 2, '1'] = count_1
+        df.at[i + 2, '-1'] = count_neg1
+        df.at[i + 2, '2'] = count_2
+        df.at[i + 2, '-2'] = count_neg2
+        df.at[i + 2, '3'] = count_3
+        df.at[i + 2, '-3'] = count_neg3
+        df.at[i + 2, '4'] = count_4
+        df.at[i + 2,'-4'] = count_neg4
 
     # Printing dataframe df
     print(df)
