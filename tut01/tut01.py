4d95cd4794dc34ddd74a057080d6ab9ca39deb8b
Aman Kumar <amankumar33152@gmail.com>
2022-09-14 18:47:39 +0530

    Successfully calculated octant value for entire dataframe

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -5,6 +5,27 @@ from platform import python_version
 # It is simpler and easy to use
 import pandas as pd
 
+# Function to find the octant value and label it for a particular row
+# taken row as an input
+def label_octant (row):
+    if row['U'] >= 0 and row["V"] >= 0 and row["W"] >= 0:
+        return 1
+    elif row['U'] < 0 and row["V"] >= 0 and row["W"] >= 0:
+        return 2
+    elif row['U'] < 0 and row["V"] < 0 and row["W"] >= 0:
+        return 3
+    elif row['U'] >= 0 and row["V"] < 0 and row["W"] >= 0:
+        return 4
+    elif row['U'] >= 0 and row["V"] >= 0 and row["W"] < 0:
+        return -1
+    elif row['U'] < 0 and row["V"] >= 0 and row["W"] < 0:
+        return -2
+    elif row['U'] < 0 and row["V"] < 0 and row["W"] < 0:
+        return -3
+    elif row['U'] >= 0 and row["V"] < 0 and row["W"] < 0:
+        return -4
+
+# Original function for octant identification
 def octact_identification(mod = 5000):
     # Loading input csv file into a dataframe (df)
     df = pd.read_csv('C:/Users/DELL/Desktop/Python Course/2001EE05_2022/tut01/octant_input.csv')
@@ -18,6 +39,13 @@ def octact_identification(mod = 5000):
     df["U"] = df["U"] - U_avg
     df["V"] = df["V"] - V_avg
     df["W"] = df["W"] - W_avg
+
+    # Constructing a new column of Octant and applying logic given below
+    # Using lambda function on a particular row to find its octant via pre defined function (label_octant())
+    df["Octant"] = df.apply(lambda row: label_octant(row), axis = 1)
+
+    # Printing dataframe df
+    print(df)
 
 
 ver = python_version()
