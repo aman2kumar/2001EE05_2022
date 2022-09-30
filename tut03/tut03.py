69765af84fa6fb3a1806eaa01e4f1e4d6b0ab098
Aman Kumar <amankumar33152@gmail.com>
2022-09-30 13:02:50 +0530

    Calculated averages and their respective velocities

--- tut03/tut03.py
+++ tut03/tut03.py
@@ -9,8 +9,36 @@ import pandas as pd
 from datetime import datetime
 start_time = datetime.now()
 
+
+# Loading input csv file into a dataframe (df)
+file = "input_octant_longest_subsequence.xlsx"
+df = pd.read_excel(file)
+
+# Calculating averages of U, V and W
+U_avg = df["U"].mean()
+V_avg = df["V"].mean()
+W_avg = df["W"].mean()
+
+# Adding Columns of respective averages calculated above in first row
+df.loc[df.index[0], 'U Avg'] = U_avg
+df.loc[df.index[0], 'V Avg'] = V_avg
+df.loc[df.index[0], 'W Avg'] = W_avg
+
+# Done preprocessing by subtracting averages from original U, V and W
+# and forming new columns respectively
+df["U' = U - U_avg"] = df["U"] - U_avg
+df["V' = V - V_avg"] = df["V"] - V_avg
+df["W' = W - W_avg"] = df["W"] - W_avg
+
+
 def octant_longest_subsequence_count():
+    print("Thinking what to code")
     # code
+    # Printing dataframe df
+    print(df)
+
+    # Forming csv file from the dataframe
+    df.to_excel("output_octant_longest_subsequence.xlsx", index = False)
 
 ver = python_version()
 
