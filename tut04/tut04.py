1d933ceeddeb37831b52505cd8f1d54da317c542
Aman Kumar <amankumar33152@gmail.com>
2022-10-09 17:40:04 +0530

    Calculated averages and respective velocities

--- tut04/tut04.py
+++ tut04/tut04.py
@@ -1,12 +1,73 @@
+# Importing different modules
+from platform import python_version
 
+# Pandas is used for working on datasets
+# It is simpler and easy to use
+import pandas as pd
+
+# DateTime module to measure the execution time of the program
 from datetime import datetime
 start_time = datetime.now()
 
-#Help https://youtu.be/H37f_x4wAC0
-def octant_longest_subsequence_count_with_range():
-###Code
+# Function to find the octant value and label it for a particular row
+# taken row as an input
+def label_octant (row):
+    if row["U' = U - U_avg"] >= 0 and row["V' = V - V_avg"] >= 0 and row["W' = W - W_avg"] >= 0:
+        return 1
+    elif row["U' = U - U_avg"] < 0 and row["V' = V - V_avg"] >= 0 and row["W' = W - W_avg"] >= 0:
+        return 2
+    elif row["U' = U - U_avg"] < 0 and row["V' = V - V_avg"] < 0 and row["W' = W - W_avg"] >= 0:
+        return 3
+    elif row["U' = U - U_avg"] >= 0 and row["V' = V - V_avg"] < 0 and row["W' = W - W_avg"] >= 0:
+        return 4
+    elif row["U' = U - U_avg"] >= 0 and row["V' = V - V_avg"] >= 0 and row["W' = W - W_avg"] < 0:
+        return -1
+    elif row["U' = U - U_avg"] < 0 and row["V' = V - V_avg"] >= 0 and row["W' = W - W_avg"] < 0:
+        return -2
+    elif row["U' = U - U_avg"] < 0 and row["V' = V - V_avg"] < 0 and row["W' = W - W_avg"] < 0:
+        return -3
+    elif row["U' = U - U_avg"] >= 0 and row["V' = V - V_avg"] < 0 and row["W' = W - W_avg"] < 0:
+        return -4
 
-from platform import python_version
+try:
+    # Loading input file into a dataframe (df)
+    file = "input_octant_longest_subsequence_with_range.xlsx"
+    df = pd.read_excel(file)
+    # Above line can have error if input file has wrong format
+    # other than .xlsx
+
+except Exception as e:  # Exception = openpyxl.utils.exceptions.InvalidFileException
+    print("There was some error due to " + str(e))
+
+# Calculating averages of U, V and W
+U_avg = df["U"].mean()
+V_avg = df["V"].mean()
+W_avg = df["W"].mean()
+
+# Adding Columns of respective averages calculated above in first row
+df.loc[df.index[0], 'U Avg'] = U_avg
+df.loc[df.index[0], 'V Avg'] = V_avg
+df.loc[df.index[0], 'W Avg'] = W_avg
+
+# Done preprocessing by subtracting averages from original U, V and W
+# and forming new columns respectively
+df["U' = U - U_avg"] = df["U"] - U_avg
+df["V' = V - V_avg"] = df["V"] - V_avg
+df["W' = W - W_avg"] = df["W"] - W_avg
+
+def octant_longest_subsequence_count_with_range():
+    # Printing dataframe df
+    print(df)
+
+    try:
+        # Forming excel file from the dataframe
+        df.to_excel("output_octant_longest_subsequence_with_range.xlsx", index = False)
+        # Above line can have error if the workbook has already been saved
+
+    except Exception as e:  # Exception
+        print("There was some error due to " + str(e))
+
+
 ver = python_version()
 
 if ver == "3.8.10":
@@ -16,13 +77,6 @@ else:
 
 
 octant_longest_subsequence_count_with_range()
-
-
-
-
-
-
-
 
 #This shall be the last lines of the code.
 end_time = datetime.now()
