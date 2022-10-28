aea26e66cf4cd1f34dbcfa873ea7abc15d4aa499
Aman Kumar <amankumar33152@gmail.com>
2022-10-28 18:57:55 +0530

    Calculated averages of U, V and W

--- tut05/tut05.py
+++ tut05/tut05.py
@@ -1,15 +1,60 @@
+# Importing different modules
+from platform import python_version
 
+# Pandas is used for working on datasets
+# It is simpler and easy to use
+import pandas as pd
 
+# DateTime module to measure the execution time of the program
 from datetime import datetime
 start_time = datetime.now()
 
-#Help https://youtu.be/N6PBd4XdnEw
-def octant_range_names(mod=5000):
+# Function to find the octant value and label it for a particular row
+# taken row as an input
+def label_octant (row):
+    if row["U' = U - U_avg"] >= 0 and row["V' = V - V_avg"] >= 0 and row["W' = W - W_avg"] >= 0:
+        return 1
+    elif row["U' = U - U_avg"] < 0 and row["V' = V - V_avg"] >= 0 and row["W' = W - W_avg"] >= 0:
+        return 2
+    elif row["U' = U - U_avg"] < 0 and row["V' = V - V_avg"] < 0 and row["W' = W - W_avg"] >= 0:
+        return 3
+    elif row["U' = U - U_avg"] >= 0 and row["V' = V - V_avg"] < 0 and row["W' = W - W_avg"] >= 0:
+        return 4
+    elif row["U' = U - U_avg"] >= 0 and row["V' = V - V_avg"] >= 0 and row["W' = W - W_avg"] < 0:
+        return -1
+    elif row["U' = U - U_avg"] < 0 and row["V' = V - V_avg"] >= 0 and row["W' = W - W_avg"] < 0:
+        return -2
+    elif row["U' = U - U_avg"] < 0 and row["V' = V - V_avg"] < 0 and row["W' = W - W_avg"] < 0:
+        return -3
+    elif row["U' = U - U_avg"] >= 0 and row["V' = V - V_avg"] < 0 and row["W' = W - W_avg"] < 0:
+        return -4
+
+try:
+    # Loading input file into a dataframe (df)
+    file = "input_octant_longest_subsequence_with_range.xlsx"
+    df = pd.read_excel(file)
+    # Above line can have error if input file has wrong format
+    # other than .xlsx
+
+except Exception as e:  # Exception = openpyxl.utils.exceptions.InvalidFileException
+    print("There was some error due to " + str(e))
+
+# Calculating averages of U, V and W
+U_avg = df["U"].mean()
+V_avg = df["V"].mean()
+W_avg = df["W"].mean()
+
+# Adding Columns of respective averages calculated above in first row
+df.loc[df.index[0], 'U Avg'] = U_avg
+df.loc[df.index[0], 'V Avg'] = V_avg
+df.loc[df.index[0], 'W Avg'] = W_avg
+
+def octant_range_names(mod = 5000):
 
     
     octant_name_id_mapping = {"1":"Internal outward interaction", "-1":"External outward interaction", "2":"External Ejection", "-2":"Internal Ejection", "3":"External inward interaction", "-3":"Internal inward interaction", "4":"Internal sweep", "-4":"External sweep"}
 
-###Code
+
 
 from platform import python_version
 ver = python_version()
@@ -20,11 +65,11 @@ else:
     print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
 
 
-mod=5000 
+mod = 5000 
 octant_range_names(mod)
 
 
 
-#This shall be the last lines of the code.
+# This shall be the last lines of the code.
 end_time = datetime.now()
 print('Duration of Program Execution: {}'.format(end_time - start_time))
